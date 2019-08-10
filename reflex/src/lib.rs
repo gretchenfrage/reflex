cc69d51ccc01e541050001dc1d9a67f327ec7c92
Phoenix Kahlo <kahlo.phoenix@gmail.com>
2019-08-09 23:42:19 -0400

    refactor code into msg_union module

--- reflex/src/lib.rs
+++ reflex/src/lib.rs
@@ -1,4 +1,9 @@
 
+/// Union types for messages which actors can process.
+pub mod msg_union;
+
+use msg_union::MessageUnion;
+
 /// Actor types.
 pub trait Actor: Sized + Send + Sync + 'static {
     /// Union type for messages which this actor can process.
@@ -15,46 +20,3 @@ pub trait ReactMut<Msg>: Actor {
     // TODO: fn process with actor guard
 }
 
-/// Trait binding together a set of message types which an actor can process,
-/// both shared and mut.
-///
-/// Implementations are meant to be created with macros.
-///
-/// Currently, this only uses associated types to bundle the shared and mut message
-/// union types into a single type parameter.
-///
-/// #### Type Parameters
-/// * `Act` - the actor type which processes this message.
-pub trait MessageUnion<Act>: Sized + Send + Sync + Copy + 'static{
-
-    /// Associated shared message union type.
-    type Shared: MessageUnionShared<Act>;
-
-    /// Associated mut message union type.
-    type Mut: MessageUnionMut<Act>;
-}
-
-/// Trait binding together a set of message for which an actor implements `ReactShared`.
-///
-/// #### Type Parameters
-/// * `Act` - the actor type which processes this message.
-pub trait MessageUnionShared<Act>: Message {
-    // TODO: fn process with actor guard
-}
-
-/// Trait binding together a set of message for which an actor implements `ReactMut`.
-///
-/// #### Type Parameters
-/// * `Act` - the actor type which processes this message.
-pub trait MessageUnionMut<Act>: Message {
-    // TODO: fn process with actor guard
-}
-
-/// Types which can be valid messages.
-///
-/// Implementations are meant to be created with macros.
-///
-/// Currently a blanket-impl for anything `Send` and `'static`.
-pub trait Message: Send + 'static {}
-
-impl<T: Send + 'static> Message for T {}
---
+++ reflex/src/msg_union.rs
@@ -0,0 +1,44 @@
+
+/// Trait binding together a set of message types which an actor can process,
+/// both shared and mut.
+///
+/// Implementations are meant to be created with macros.
+///
+/// Currently, this only uses associated types to bundle the shared and mut message
+/// union types into a single type parameter.
+///
+/// #### Type Parameters
+/// * `Act` - the actor type which processes this message.
+pub trait MessageUnion<Act>: Sized + Send + Sync + Copy + 'static{
+
+    /// Associated shared message union type.
+    type Shared: MessageUnionShared<Act>;
+
+    /// Associated mut message union type.
+    type Mut: MessageUnionMut<Act>;
+}
+
+/// Trait binding together a set of message for which an actor implements `ReactShared`.
+///
+/// #### Type Parameters
+/// * `Act` - the actor type which processes this message.
+pub trait MessageUnionShared<Act>: Message {
+    // TODO: fn process with actor guard
+}
+
+/// Trait binding together a set of message for which an actor implements `ReactMut`.
+///
+/// #### Type Parameters
+/// * `Act` - the actor type which processes this message.
+pub trait MessageUnionMut<Act>: Message {
+    // TODO: fn process with actor guard
+}
+
+/// Types which can be valid messages.
+///
+/// Implementations are meant to be created with macros.
+///
+/// Currently a blanket-impl for anything `Send` and `'static`.
+pub trait Message: Send + 'static {}
+
+impl<T: Send + 'static> Message for T {}
